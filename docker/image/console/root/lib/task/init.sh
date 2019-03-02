cd46a9d94321ed14339b077b0ebda191ec4d5bf9
dcole-inviqa <dcole@inviqa.com>
2019-03-02 14:08:07 +0000

    Refactoring and fixes

--- docker/image/console/root/lib/task/init.sh
+++ docker/image/console/root/lib/task/init.sh
@@ -3,7 +3,6 @@
 function task_init()
 {
     task "mysql:available"
-    task "wordpress:configfile"
     task "assets:apply"
     task "welcome"
 }
--- docker/image/console/root/lib/task/wordpress/install.sh
+++ docker/image/console/root/lib/task/wordpress/install.sh
@@ -4,15 +4,19 @@ function task_wordpress_install()
 {
     task "mysql:available"
 
+    passthru "bin/wp-cli.phar core download \
+        --path='${WORDPRESS_INSTALL_DIRECTORY}'"
+
+    task "skeleton:apply"
+
     passthru "bin/wp-cli.phar core install \
         --path='${WORDPRESS_INSTALL_DIRECTORY}' \
         --skip-email \
-        --url='${WORDPRESS_URL}' \
+        --url='$(eval echo $WORDPRESS_URL)' \
         --title='${APP_NAME}' \
         --admin_user=admin \
         --admin_password=admin123 \
         --admin_email=admin@localhost.local"
 
-    task "skeleton:apply"
     task "assets:dump"
 }
